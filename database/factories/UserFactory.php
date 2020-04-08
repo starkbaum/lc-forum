1e572c578d29c83f73e6daa03de3242358b10b08
starkbaum <starkbaum.stefan@gmail.com>
2020-04-08 15:44:10 +0200

    Lesson 001 - Initial Database Setup With Seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Thread;
+use App\Reply;
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -24,5 +26,27 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+    ];
+});
+
+$factory->define(Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
+        'thread_id' => function() {
+            return factory(Thread::class)->create()->id;
+        },
+        'body' => $faker->paragraph,
     ];
 });
